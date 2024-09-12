4efa4ee07c1edfec09b66e069a07643c78ba4a28
Victor Barbosa <barbosahvictor@gmail.com>
2024-09-12 07:03:03 -0400

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import fs from 'fs';
+import chalk from 'chalk';
+
+function extraiLinks(texto) {
+    const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+    const capturas = [...texto.matchAll(regex)];
+    const resultados = capturas.map(captura => ({[captura[1]]: captura[2]}));
+    return resultados.length !== 0 ? resultados : 'Não há links no arquivo';
+}
+
+function trataErro(erro) {
+    console.log(erro);
+    throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'));
+}
+
+// promises com async/await
+async function pegaArquivo(caminhoDoArquivo) {
+    try {
+        const encoding = 'utf-8';
+        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
+        return extraiLinks(texto);
+    } catch(erro) {
+        trataErro(erro)
+    } finally {
+        console.log(chalk.yellow('operação concluída'));
+    }
+}
+
+export default pegaArquivo;
